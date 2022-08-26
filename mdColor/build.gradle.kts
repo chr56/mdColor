51dc6dbeba3b11ba96090f5f16ed5c93180f1345
chr56 <_>
2022-08-26 10:13:00 +0800

    [Gradle] use kotlin DSL

---
+++ mdColor/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("com.android.library")
+    id("maven-publish")
+}
+val libVersion = "0.0.1"
+
+android {
+    compileSdk = 33
+    buildToolsVersion = "33.0.0"
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 33
+
+        consumerProguardFiles.add(File("consumer-rules.pro"))
+
+        aarMetadata {
+            minCompileSdk = 23
+        }
+    }
+
+    sourceSets {
+        named("main") {
+            java.srcDir("scr/main/java")
+            res.srcDir("scr/main/res")
+        }
+    }
+
+    buildFeatures {
+        buildConfig = false
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles.apply {
+                add(getDefaultProguardFile("proguard-android-optimize.txt"))
+                add(File("proguard-rules.pro"))
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    publishing {
+        publishing {
+            singleVariant("release") {
+                withSourcesJar()
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.annotation:annotation:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            groupId = "com.github.chr56"
+            artifactId = "mdColor"
+            version = libVersion
+        }
+    }
+}
